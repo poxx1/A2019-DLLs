57b330c18697fb315eb7665b2fd00a2012bbdadf
poxx1 <julianlastrama@outlook.com>
2021-03-09 15:08:23 -0300

    First push

---
+++ HTMLtoTXT/Conversor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace HTMLtoTXT
+{
+    public class Conversor
+    {
+        public void Convert(string input,string output)
+        {
+            StreamReader srd = new StreamReader(input);
+            var txt = srd.ReadToEnd();
+            StreamWriter sw = new StreamWriter(output);
+            sw.Write(txt);
+            sw.Close();
+        }
+    }
+}
