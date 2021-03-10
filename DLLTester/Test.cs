47537c7587d8b5825cf74bc2b30df66cd7bebf2f
poxx1 <julianlastrama@outlook.com>
2021-03-10 09:37:19 -0300

    Modificacion Full > Test Pending

--- DLLTester/Test.cs
+++ DLLTester/Test.cs
@@ -12,10 +12,10 @@ namespace DLLTester
             //Para hacer la prueba de la DLL, hay que instanciar la clase a usar y ejecutar el metodo deseado. EJ:
             //Fundamental agregar la referencia a este proyecto, ya que sino la clase no sera visible.
             Conversor convert = new Conversor();
-            convert.Convert("ruta1","ruta2");
+            Console.WriteLine(convert.FechaPago(@"C:\Users\julian.lastra\Desktop\asd.html"));
 
             //En caso de que me tenga que entregar un resultado tipo int o string uso la siguiente forma:
-            Console.WriteLine(convert.Extract("texto"));
+            //Console.WriteLine(convert.Extract("texto"));
         }
     }
 }
--- HTMLtoTXT/Conversor.cs
+++ HTMLtoTXT/Conversor.cs
@@ -6,11 +6,47 @@ namespace HTMLtoTXT
     public class Conversor
     {
         //Extraigo texto en particular que necesite desde un archivo
-        public string Extract(string input)
+        public string NumeroComprobante(string input)
         {
             StreamReader sr = new StreamReader(input);
-            var num = sr.ReadToEnd().Split(new[] { "texto" }, StringSplitOptions.None);
-            return num[0];
+            var num = sr.ReadToEnd().Split(new[] { "<TD class=b>" }, StringSplitOptions.None);
+            var fechaPago = num[1].Split(new[] { "</TD>" }, StringSplitOptions.None);
+            return fechaPago[0];
+        }
+        public string FechaPago(string input)
+        {
+            StreamReader sr = new StreamReader(input);
+            var num = sr.ReadToEnd().Split(new[] { "FECHA DE PAGO</TD>" }, StringSplitOptions.None);
+            var fechaPago = num[1].Split(new[] { "</TD>" }, StringSplitOptions.None);
+            return fechaPago[0].Replace("<TD width=20%>","");
+        }
+        public string Recaudacion(string input)
+        {
+            StreamReader sr = new StreamReader(input);
+            var num = sr.ReadToEnd().Split(new[] { "RECAUDACION NETA</TD>" }, StringSplitOptions.None);
+            var fechaPago = num[1].Split(new[] { "</TD>" }, StringSplitOptions.None);
+            return fechaPago[0].Replace("<TD width=20%>", "");
+        }
+        public string Comision(string input)
+        {
+            StreamReader sr = new StreamReader(input);
+            var num = sr.ReadToEnd().Split(new[] { "=tt>COMISION</TD>" }, StringSplitOptions.None);
+            var fechaPago = num[1].Split(new[] { "</TD>" }, StringSplitOptions.None);
+            return fechaPago[0].Replace("<TD width=20%>", "");
+        }
+        public string Percepcion(string input)
+        {
+            StreamReader sr = new StreamReader(input);
+            var num = sr.ReadToEnd().Split(new[] { "RG 3337</TD>" }, StringSplitOptions.None);
+            var fechaPago = num[1].Split(new[] { "</TD>" }, StringSplitOptions.None);
+            return fechaPago[0];//.Replace("<TD width=20%>", "");
+        }
+        public string IVAs(string input)
+        {
+            StreamReader sr = new StreamReader(input);
+            var num = sr.ReadToEnd().Split(new[] { "I.V.A. S/COMISION</TD>" }, StringSplitOptions.None);
+            var fechaPago = num[1].Split(new[] { "</TD>" }, StringSplitOptions.None);
+            return fechaPago[0].Replace("<TD width=20%>", "");
         }
 
         //Tomo un .html lo convierto a .txt y lo guardo en alguna ubicacion.
